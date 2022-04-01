19c366d0148bdcc96f6189e62cac3d1befd80bd5
Aiden Martinez <Angrybull2007@gmail.com>
2022-04-01 13:05:25 -0600

    Initial Commit

---
+++ Alert/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alert
+//
+//  Created by Aiden Martinez on 4/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
