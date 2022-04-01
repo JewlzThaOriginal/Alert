f06df08d76ab1ac6f0d731c22cb3ad4bb7135984
Aiden Martinez <Angrybull2007@gmail.com>
2022-04-01 14:20:05 -0600

    MVP

--- Alert/ViewController.swift
+++ Alert/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+        
+    @IBAction func alertButton(_ sender: Any) {
+        let mySet = CharacterSet(["0","1","2","3","4","5","6","7","8","9"])
+        
+        var text1 = textField.text ?? ""
+        if text1.rangeOfCharacter(from: mySet) != nil { let alert = UIAlertController(title: "Please Enter Only Letters!", message: nil, preferredStyle: .alert)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            let ok = UIAlertAction(title: "OK", style: .default) { action in print("OK Pressed")
+                
+            }
+            alert.addAction(ok)
+            
+            let cancel = UIAlertAction(title: "Cancel",style: .cancel)
+            alert.addAction(cancel)
+        }
+    }
+    
+    
+    }
+    
 
 
-}
 
